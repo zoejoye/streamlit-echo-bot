f21ea7ddf84f5145c44ad7c857c02ea563af8267
zoejoye <zoethinkera@gmail.com>
2026-03-06 13:05:56 -0600

    Update echobot.py

--- echobot.py
+++ echobot.py
@@ -1,5 +1,6 @@
 import streamlit as st
+import numpy as np
 
 with st.chat_message("assistant"):
-  st.write("Hello, how's it going? 👋")
-  
+    st.write("Hello human")
+    st.bar_chart(np.random.randn(30, 3))
