e676e5c7dd3083eb2856133b7fd3b2903fec9123
zoejoye <zoethinkera@gmail.com>
2026-03-06 13:33:28 -0600

    Fix indentation issues in echobot.py

--- echobot.py
+++ echobot.py
@@ -1,29 +1,30 @@
-import streamlit as st
+    import streamlit as st
 
-st.title("Echo Bot")
+    st.title("Echo Bot")
 
-# Initialize chat history
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
 
-# Display chat messages from history on app rerun
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    # Display chat messages from history on app rerun
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
-# React to user input
-if prompt := st.chat_input("What is up?"):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
+    # React to user input
+    if prompt := st.chat_input("What is up?"):
+        # Display user message in chat message container
+        st.chat_message("user").markdown(prompt)
 
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
-    response = f"Echo: {prompt}"
-    # Display assistant response in chat message container
+        response = f"Echo: {prompt}"
+        # Display assistant response in chat message container
+        
+        with st.chat_message("assistant"):
+            st.markdown(response)
+        
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
     
-    with st.chat_message("assistant"):
-        st.markdown(response)
-    
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": response})
