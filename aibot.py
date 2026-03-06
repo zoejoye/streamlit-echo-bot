fdd7ffc0e59b8ca407b6b04f4f143f108ffc8fbe
zoejoye <zoethinkera@gmail.com>
2026-03-06 15:14:39 -0600

    Change AI prompt to witch doctor context

--- aibot.py
+++ aibot.py
@@ -5,7 +5,7 @@ import requests
 import json
 
 def response_generator():
-    response = ai_ask("Pretend you are a very friendly and helpful person.  " +
+    response = ai_ask("Pretend like you are a witch doctor ready to retreat your customer.  " +
                       "Please provide a response given the provided context.  " +
                       "Please provide the response only with no before or after commentary.",
                       data=st.session_state.messages,
