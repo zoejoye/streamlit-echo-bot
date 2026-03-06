f8034c15e18a60bfb0871ae279a838931acdfd6b
zoejoye <zoethinkera@gmail.com>
2026-03-06 13:18:02 -0600

    Refactor echobot to use chat input
    Removed numpy dependency and updated chat message handling.

--- echobot.py
+++ echobot.py
@@ -1,6 +1,5 @@
 import streamlit as st
-import numpy as np
 
-with st.chat_message("assistant"):
-    st.write("Hello human")
-    st.bar_chart(np.random.randn(30, 3))
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
